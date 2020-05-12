4ea13e02413c07fc2379ca968cadfea1f52febc6
Jacob Prudent <jake.prudent@gmail.com>
2020-05-11 18:56:03 -0700

    MVP for Queue and Stack

--- queue/queue.py
+++ queue/queue.py
@@ -10,16 +10,61 @@ return elements in First In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
 """
+
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return (len(self.storage))
+
+#     def enqueue(self, value):
+#         self.size += 1
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         if self.size > 0:
+#             self.size -= 1
+#             return self.storage.pop(0)
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class Queue:
-    def __init__(self):
-        self.size = 0
-        # self.storage = ?
-    
+    def __init__(self, node=None):
+        self.length = 0
+        self.head = node
+        self.tail = node
+
     def __len__(self):
-        pass
+        return self.length
 
     def enqueue(self, value):
-        pass
+        self.length += 1
+        new_node = Node(value)
+        if self.head:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
 
     def dequeue(self):
-        pass
+        if self.head:
+            if self.head == self.tail:
+                current_tail = self.tail
+                self.length -= 1
+                self.head = None
+                self.tail = None
+                return current_tail.value
+
+            else:
+                self.length -= 1
+                current = self.head
+                self.head = self.head.next
+                return current.value
--- stack/stack.py
+++ stack/stack.py
@@ -9,6 +9,7 @@ return elements in Last In First Out order.
    Make sure the Stack tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
+   How it is stored in mememory. It is also easier to add additional attributes
 """
 
 # This one uses an array
@@ -29,4 +30,48 @@ return elements in Last In First Out order.
 #         return None
 
 # This one uses a linked list
-from doubly_linked_list import
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+class Stack:
+    def __init__(self, node=None):
+        self.length = 0
+        self.head = node
+        self.tail = node
+
+    def __len__(self):
+        return self.length
+
+    def push(self, value):
+        self.length += 1
+        new_node = Node(value)
+        if self.head:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+
+    def pop(self):
+        if self.head:
+            if self.head == self.tail:
+                current_tail = self.tail
+                self.length -= 1
+                self.head = None
+                self.tail = None
+                return current_tail.value
+
+            else:
+                self.length -= 1
+                current = self.head
+                last = self.tail
+                while current.next != last:
+                    current = current.next
+                current.next = None
+                self.tail = current
+                return last.value
